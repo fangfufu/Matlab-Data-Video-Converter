614ae91409b9355f0ff783c5f008b90b7d1dac95
Fufu Fang <fangfufu2003@gmail.com>
2015-01-28 12:33:32 +0000

    renamed a function

--- VideoEncode.m
+++ VideoEncode.m
@@ -2,6 +2,6 @@ function [dataLength] = VideoEncode( in_name, out_name )
 %VIDEOENCODE Convert a file into a video
 %   Detailed explanation goes here
 [dataArray, dataLength] = DataFramer(FileReader(in_name));
-ArrayDumper(dataArray, strcat(out_name, '_', num2str(dataLength)));
+ArrayWriter(dataArray, strcat(out_name, '_', num2str(dataLength)));
 end
 

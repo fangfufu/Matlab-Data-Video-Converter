ac1adc06900e8f01d6b18a3a45ac5d5ed8ebf364
Fufu Fang <fangfufu2003@gmail.com>
2015-02-06 23:16:01 +0000

    added some shell script for the conversion

---
+++ youtube2file.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BLOCK_SIZE=16
+FRAME_WIDTH_MP4=1920
+FRAME_HEIGHT_MP4=1080
+
+FRAME_WIDTH=$(echo "${FRAME_WIDTH}/${BLOCK_SIZE}" | bc)
+FRAME_HEIGHT=$(echo "${FRAME_HEIGHT}/${BLOCK_SIZE}" | bc)
+MP4_HZ=25
+MP4_FPS=25
+
+youtube-dl $1 -f 137 -o - | ffmpeg -y -i pipe:0 -r ${MP4_FPS} -f rawvideo -pix_fmt monob -vf "scale=iw/${BLOCK_SIZE}:ih/${BLOCK_SIZE}" -video_size ${FRAME_WIDTH}x${FRAME_HEIGHT} -sws_flags neighbor -r ${MP4_HZ} pipe:1 | xz -d > $2

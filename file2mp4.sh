90a18be5a0795af02be18bc43ed7954622289387
Fufu Fang <fangfufu2003@gmail.com>
2015-02-09 04:55:40 +0000

    reverted some changes

--- file2mp4.sh
+++ file2mp4.sh
@@ -6,9 +6,9 @@ FRAME_HEIGHT_MP4=1080
 
 FRAME_WIDTH=$(echo "${FRAME_WIDTH_MP4}/${BLOCK_SIZE}" | bc)
 FRAME_HEIGHT=$(echo "${FRAME_HEIGHT_MP4}/${BLOCK_SIZE}" | bc)
-DATA_FPS=60
-VID_FPS=60
+MP4_HZ=60
+MP4_FPS=60
 
 FILE_SIZE=$(stat -c%s "$1")
 NEXT_SIZE=$(echo "($FRAME_WIDTH*$FRAME_HEIGHT)" | bc)
-{ xz -1 -k $1 --stdout; dd if=/dev/zero bs=$NEXT_SIZE count=1; } | ffmpeg -y -f rawvideo -pix_fmt monob -video_size ${FRAME_WIDTH}x${FRAME_HEIGHT} -r ${DATA_FPS} -i "pipe:0" -vf scale=iw*${BLOCK_SIZE}:ih*${BLOCK_SIZE} -sws_flags neighbor -r ${VID_FPS} -pix_fmt yuv420p -preset superfast "$2"
+{ xz -1 -k $1 --stdout; dd if=/dev/zero bs=$NEXT_SIZE count=1; } | ffmpeg -y -f rawvideo -pix_fmt monob -video_size ${FRAME_WIDTH}x${FRAME_HEIGHT} -r ${MP4_HZ} -i "pipe:0" -vf scale=iw*${BLOCK_SIZE}:ih*${BLOCK_SIZE} -sws_flags neighbor -r ${MP4_FPS} -pix_fmt yuv420p -preset superfast "$2"
--- mp42file.sh
+++ mp42file.sh
@@ -3,10 +3,9 @@
 BLOCK_SIZE=16
 FRAME_WIDTH_MP4=1920
 FRAME_HEIGHT_MP4=1080
+FRAME_WIDTH=$(echo "${FRAME_WIDTH}/${BLOCK_SIZE}" | bc)
+FRAME_HEIGHT=$(echo "${FRAME_HEIGHT}/${BLOCK_SIZE}" | bc)
+MP4_HZ=60
+MP4_FPS=60
 
-FRAME_WIDTH=$(echo "${FRAME_WIDTH_MP4}/${BLOCK_SIZE}" | bc)
-FRAME_HEIGHT=$(echo "${FRAME_HEIGHT_MP4}/${BLOCK_SIZE}" | bc)
-DATA_FPS=60
-VID_FPS=60
-
-ffmpeg -y -i "$1" -r ${VID_FPS} -f rawvideo -pix_fmt monow -vf "scale=iw/${BLOCK_SIZE}:ih/${BLOCK_SIZE}" -video_size ${FRAME_WIDTH}x${FRAME_HEIGHT} -sws_flags neighbor -r ${DATA_FPS} $2 | xz -d --stdout > $2
+ffmpeg -y -i "$1" -r ${MP4_FPS} -f rawvideo -pix_fmt monob -vf "scale=iw/${BLOCK_SIZE}:ih/${BLOCK_SIZE}" -video_size ${FRAME_WIDTH}x${FRAME_HEIGHT} -sws_flags neighbor -r ${MP4_HZ} pipe:1 | xz -d --stdout > $2
